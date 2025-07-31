55d1f7a8017ca182c01fec404b48a8959f7d4986
birdmichael <birdmichael126@gmail.com>
2025-08-01 04:55:37 +0800

    初始化项目工程

---
+++ Oura/App/OuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  OuraApp.swift
+//  Oura
+//
+//  Created by BM on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct OuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Oura/Views/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Oura
+//
+//  Created by BM on 8/1/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        Text("demo")
+    }
+}
+
+#Preview {
+    ContentView()
+}
